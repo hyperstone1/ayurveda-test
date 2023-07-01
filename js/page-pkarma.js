99e95b8858096ab1f67a0691667937534de32def
hyperstone1 <kingofthegorgon@mail.ru>
2023-07-01 19:34:02 +0300

    add panchakarma page

--- js/page-pkarma.js
+++ js/page-pkarma.js
@@ -1,3 +1,19 @@
-$(document).ready(function(){
+const rem = function (rem) {
+  if (window.innerWidth > 768) {
+    return 0.005208335 * window.innerWidth * rem;
+  } else {
+    // где 375 это ширина моб версии макета
+    return (100 / 375) * (0.1 * window.innerWidth) * rem;
+  }
+};
 
+const contraindications = new Swiper('.contraindications__list', {
+  slidesPerView: 'auto',
+  spaceBetween: rem(10),
+  centeredSlides: false,
+  navigation: {
+    nextEl: '.navigation-next',
+    prevEl: '.navigation-prev',
+  },
+  // loop: true,
 });
